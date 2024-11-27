47c21e6ac79e5e223600f09ceac9450e8e042f40
AMagicPear <AMagicPear@outlook.com>
2024-11-27 17:23:57 +0800

    爬取歌曲部分debug

--- theorytab_crawler.py
+++ theorytab_crawler.py
@@ -12,11 +12,10 @@ website_url = "https://www.hooktheory.com"
 artists_entry_url = website_url + "/theorytab/artists/"
 sleep_time = 0.11
 alphabet_list = string.ascii_lowercase
-# alphabet_list = "x"
 root_dir = "./datasets"
 root_info = os.path.join(root_dir, "info")
 url_cache = "./datasets/cache"
-url_artists_cache_file = os.path.join(url_cache,'url_artist_list_cache.pkl')
+url_artists_cache_file = os.path.join(url_cache, "url_artist_list_cache.pkl")
 
 # 创建一个带有重试策略的会话
 session = requests.Session()
@@ -76,22 +75,49 @@ def fetch_artist_songs(artist_link: str, cache_file: str = None):
 
     print(f"正在爬取艺人 {artist_link.split('/')[-1]} 的歌曲：")
     response_tmp = session.get(website_url + artist_link)
-    soup = BeautifulSoup(response_tmp.text, 'html.parser')
+    soup = BeautifulSoup(response_tmp.text, "html.parser")
     item_list = soup.find_all("li", {"class": re.compile("overlay-trigger")})
     songs_name = []
     for item in item_list:
-        song_name = item.find_all("a", {"class": "a-no-decoration"})[0]['href'].split('/')[-1]
+        song_name = item.find_all("a", {"class": "a-no-decoration"})[0]["href"].split(
+            "/"
+        )[-1]
         songs_name.append(song_name)
-        print(f'   > {song_name}')
-    
+        print(f"   > {song_name}")
+
     if cache_file:
         with open(cache_file, "wb") as f:
             pickle.dump(songs_name, f)
-    
+
     return songs_name
 
 
-if __name__ == "__main__":
+def song_retrieval(artist_name: str, song_name: str):
+    """用于爬取指定艺人指定歌曲的数据"""
+    suffix = "/theorytab/view/" + artist_name + "/" + song_name
+    song_url = website_url + suffix
+    response_temp = requests.get(song_url)
+    soup = BeautifulSoup(response_temp.text, "html.parser")
+    sections = [
+        item["href"].split("#")[-1]
+        for item in soup.find_all("a", {"href": re.compile(suffix + "#")})
+    ]
+    print(f"Sections found: {sections}")
+    pks = [
+        item["href"].split("/")[-1]
+        for item in soup.find_all("a", {"href": re.compile("/theorytab/chords/pk/")})
+    ]
+    print(f"PKs found: {pks}")
+    for index, pk in enumerate(pks):
+        req_url = f"{website_url}/songs/getXmlByPk?pk={pk}"
+        response_info = requests.get(req_url)
+        with open(os.path.join(root_info,sections[index]+'.xml'),'w',encoding='utf-8') as f:
+            f.write(response_info.text)
+        time.sleep(0.08)
+
+song_retrieval("lone","hiraeth")
+
+if __name__ != "__main__":
     # 补全目录
     if not os.path.exists(root_dir):
         os.makedirs(root_dir)
@@ -102,5 +128,17 @@ if __name__ == "__main__":
         artists_entry_url, alphabet_list, url_artists_cache_file
     )
     # 获取歌曲列表
-    # print(artist_links_collection[0])
-    print(os.path.join(url_cache,'songs',artist_links_collection[0].split('/')[-1]+'.pkl'))
+    num_song = 0
+    artist_song_dict = dict()
+    for artist_link in artist_links_collection:
+        artist_name = artist_link.split("/")[-1]
+        songs_name = fetch_artist_songs(
+            artist_link, os.path.join(url_cache, "songs", artist_name + ".pkl")
+        )
+        num_song += len(songs_name)
+        artist_song_dict[artist_name] = songs_name
+        print(f"当前歌曲名单总数：{num_song}")
+    for artist_link in artist_links_collection:
+        artist_name = artist_link.split("/")[-1]
+        info_path = os.path.join(url_cache, "info")
+        # song_retrieval(artist_name,artist_song_dict[artist_name][0])
